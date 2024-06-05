a837d104b7e3a64c43cd0c6f73efc11159200f0e
rafaelGurgelS <rafaelgs@gmail.com>
2024-06-05 17:06:02 -0300

    CódigoFinal

--- RoboSumo.cpp
+++ RoboSumo.cpp
@@ -3,17 +3,16 @@
 // Definições de pinos
 #define pinoTrigger 7         // Pino Trigger do sensor ultrassônico
 #define pinoEcho 8            // Pino Echo do sensor ultrassônico
-#define pinoMotorEsquerdo1 3  // Pino 1 do motor esquerdo
-#define pinoMotorEsquerdo2 5  // Pino 2 do motor esquerdo
-#define pinoMotorDireito1 6   // Pino 1 do motor direito
-#define pinoMotorDireito2 9   // Pino 2 do motor direito
+#define pinoMotorEsquerdo1 5  // Pino 1 do motor esquerdo
+#define pinoMotorEsquerdo2 3  // Pino 2 do motor esquerdo
+#define pinoMotorDireito1 9   // Pino 1 do motor direito
+#define pinoMotorDireito2 6   // Pino 2 do motor direito
 #define pinoSensorEsquerdo 11 // Pino do sensor de linha esquerdo
 #define pinoSensorDireito 12  // Pino do sensor de linha direito
 #define receptor 10           // Receptor Infravermelho
 
 // Variáveis de controle
 bool ligado = false;
-bool trava = true;
 
 // Variáveis para controle do sensor ultrassônico
 long duracao;
@@ -23,80 +22,44 @@ int distancia;
 bool sensorEsquerdoDetectado = false;
 bool sensorDireitoDetectado = false;
 
-// Variável para controle da velocidade
-int velocidade = 200; // Velocidade inicial
-
-// Inicializa o receptor IR
-IRrecv irrecv(receptor);
-decode_results results;
-
 // Lista de códigos válidos para ativar e desativar o robô
-unsigned long codigoAtivacao =  0xFC03EF00;
+unsigned long codigoAtivacao = 0xFC03EF00;
 unsigned long codigoDesativacao = 0xFD02EF00;
 
-void setup() {
-  // Inicializa comunicação serial para debug
-  Serial.begin(9600);
-
-  // Define os pinos como entrada ou saída
-  pinMode(pinoTrigger, OUTPUT);
-  pinMode(pinoEcho, INPUT);
-  pinMode(pinoMotorEsquerdo1, OUTPUT);
-  pinMode(pinoMotorEsquerdo2, OUTPUT);
-  pinMode(pinoMotorDireito1, OUTPUT);
-  pinMode(pinoMotorDireito2, OUTPUT);
-  pinMode(pinoSensorEsquerdo, INPUT);
-  pinMode(pinoSensorDireito, INPUT);
-  pinMode(receptor, INPUT);
-
-  // Inicia o receptor IR
-  irrecv.enableIRIn();
+// Funções de movimento do robô
+void frente() {
+  digitalWrite(pinoMotorEsquerdo1, HIGH);
+  digitalWrite(pinoMotorEsquerdo2, LOW);
+  digitalWrite(pinoMotorDireito1, HIGH);
+  digitalWrite(pinoMotorDireito2, LOW);
 }
 
-void loop() {
-  // Recebe sinal infravermelho
-  if (irrecv.decode()) {
-    unsigned long codigoRecebido = irrecv.decodedIRData.decodedRawData; 
-    
-    // Verifica se o código é de ativação
-    if (codigoRecebido == codigoAtivacao) {
-      ligado = true;
-      Serial.println("Ativando...");
-    }
-    // Verifica se o código é de desativação
-    if (codigoRecebido == codigoDesativacao) {
-      parar();
-      ligado = false;
-      Serial.println("Desativando...");
-    }
-    irrecv.resume(); 
-  }
-  
-  // Operação principal
-  if (ligado) {
-    // Lê os sensores de linha
-    sensorEsquerdoDetectado = digitalRead(pinoSensorEsquerdo);
-    sensorDireitoDetectado = digitalRead(pinoSensorDireito);
+void re() {
+  digitalWrite(pinoMotorEsquerdo1, LOW);
+  digitalWrite(pinoMotorEsquerdo2, HIGH);
+  digitalWrite(pinoMotorDireito1, LOW);
+  digitalWrite(pinoMotorDireito2, HIGH);
+}
 
-    // Realiza manobra para voltar à arena
-    if (!sensorEsquerdoDetectado) {
-      realizarManobraParaVoltar(true);
-    } else if (!sensorDireitoDetectado) {
-      realizarManobraParaVoltar(false);
-    } else {
-      // Lê a distância do sensor ultrassônico
-      distancia = lerDistancia();
+void virarEsquerda() {
+  digitalWrite(pinoMotorEsquerdo1, LOW);
+  digitalWrite(pinoMotorEsquerdo2, HIGH);
+  digitalWrite(pinoMotorDireito1, HIGH);
+  digitalWrite(pinoMotorDireito2, LOW);
+}
 
-      // Avança se detectar o oponente
-      if (distancia < 20) {
-        frente();
-      } else {
-        // Procura o oponente
-        procurarOponente();
-        delay(100);
-      }
-    }
-  }
+void virarDireita() {
+  digitalWrite(pinoMotorEsquerdo1, HIGH);
+  digitalWrite(pinoMotorEsquerdo2, LOW);
+  digitalWrite(pinoMotorDireito1, LOW);
+  digitalWrite(pinoMotorDireito2, HIGH);
+}
+
+void parar() {
+  digitalWrite(pinoMotorEsquerdo1, LOW);
+  digitalWrite(pinoMotorEsquerdo2, LOW);
+  digitalWrite(pinoMotorDireito1, LOW);
+  digitalWrite(pinoMotorDireito2, LOW);
 }
 
 // Função para ler a distância do sensor ultrassônico
@@ -111,71 +74,67 @@ int lerDistancia() {
   return duracao * 0.034 / 2;
 }
 
-// Funções de movimento do robô
-void frente() {
-  analogWrite(pinoMotorEsquerdo1, velocidade);
-  digitalWrite(pinoMotorEsquerdo2, LOW);
-  analogWrite(pinoMotorDireito1, velocidade);
-  digitalWrite(pinoMotorDireito2, LOW);
+void setup() {
+  // Inicializa comunicação serial para debug
+  Serial.begin(9600);
+
+  // Define os pinos como entrada ou saída
+  pinMode(pinoTrigger, OUTPUT);
+  pinMode(pinoEcho, INPUT);
+  pinMode(pinoMotorEsquerdo1, OUTPUT);
+  pinMode(pinoMotorEsquerdo2, OUTPUT);
+  pinMode(pinoMotorDireito1, OUTPUT);
+  pinMode(pinoMotorDireito2, OUTPUT);
+  pinMode(pinoSensorEsquerdo, INPUT);
+  pinMode(pinoSensorDireito, INPUT);
+
+  // Inicia o receptor IR
+  IrReceiver.begin(receptor, ENABLE_LED_FEEDBACK); 
 }
 
-void re() {
-  digitalWrite(pinoMotorEsquerdo1, LOW);
-  analogWrite(pinoMotorEsquerdo2, velocidade);
-  digitalWrite(pinoMotorDireito1, LOW);
-  analogWrite(pinoMotorDireito2, velocidade);
-}
-
-void virarEsquerda() {
-  digitalWrite(pinoMotorEsquerdo1, LOW);
-  analogWrite(pinoMotorEsquerdo2, velocidade);
-  analogWrite(pinoMotorDireito1, velocidade);
-  digitalWrite(pinoMotorDireito2, LOW);
-}
-
-void virarDireita() {
-  analogWrite(pinoMotorEsquerdo1, velocidade);
-  digitalWrite(pinoMotorEsquerdo2, LOW);
-  digitalWrite(pinoMotorDireito1, LOW);
-  analogWrite(pinoMotorDireito2, velocidade);
-}
-
-void girar() {
-  digitalWrite(pinoMotorEsquerdo1, LOW);
-  analogWrite(pinoMotorEsquerdo2, velocidade);
-  analogWrite(pinoMotorDireito1, velocidade);
-  digitalWrite(pinoMotorDireito2, LOW);
-}
-
-void parar() {
-  digitalWrite(pinoMotorEsquerdo1, LOW);
-  digitalWrite(pinoMotorEsquerdo2, LOW);
-  digitalWrite(pinoMotorDireito1, LOW);
-  digitalWrite(pinoMotorDireito2, LOW);
-}
-
-void procurarOponente() {
-  // Gira em torno do próprio eixo
-  girar();
-  delay(1000); // Ajustar tempo de giro
-  parar();
-  delay(1);
-}
-
-void realizarManobraParaVoltar(bool esquerdo) {
-  parar();
-  delay(1);
-  re();
-  delay(300);
-  parar();
-  delay(1);
-  if (esquerdo) {
-    virarDireita();
-  } else {
-    virarEsquerda();
+void loop() {
+  // Recebe sinal infravermelho
+  if (IrReceiver.decode()) {
+    unsigned long codigoRecebido = IrReceiver.decodedIRData.decodedRawData; 
+    
+    // Verifica se o código é de ativação
+    if (codigoRecebido == codigoAtivacao) {
+      ligado = true;
+      Serial.println("Ativando...");
+    } else if (codigoRecebido == codigoDesativacao) {
+      ligado = false;
+      Serial.println("Desativando...");
+    }
+    IrReceiver.resume(); 
   }
-  delay(600);
-  parar();
-  delay(1);
-  frente();
+  
+  // Operação principal
+  if (ligado) {
+  // Lê os sensores de linha
+      sensorEsquerdoDetectado = digitalRead(pinoSensorEsquerdo);
+      sensorDireitoDetectado = digitalRead(pinoSensorDireito);
+
+      // Realiza manobra para voltar à arena
+      if (!sensorEsquerdoDetectado) { //se o esquerdo ver preto
+        re(); //volta
+        delay(1000);
+      } else if (!sensorDireitoDetectado) { //se o direito ver preto
+        re();
+        delay(1000);
+      } else { //se ambos verem branco
+        // Lê a distância do sensor ultrassônico
+        distancia = lerDistancia();
+
+        // Avança se detectar o oponente
+        if (distancia < 40) {
+          frente();
+        } else {
+          // Procura o oponente
+          virarDireita();
+        }
+      }
+  } else {
+    parar(); // Para os motores se não estiver ligado
+    //delay(1000);
+  }
 }
